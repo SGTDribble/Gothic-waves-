1753c0edb344eb543e30f098162ca8d1ade6987c
SGTDribble <liam.rawlings@hotmail.com>
2025-11-20 23:03:20 +0000

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,17 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('gothic-blaster').then(cache => {
-      return cache.addAll([
-        'index.html',
-        'style.css',
-        'game.js',
-        'manifest.json'
-      ]);
-    })
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+// sw
